af34fef2aaccd29a27ffaeed12cdd5a53089cdb3
anandsa83 <65344631+anandsa83@users.noreply.github.com>
2025-09-11 10:51:13 +0530

    Changes

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -16,7 +16,7 @@ resource "aws_vpc_peering_connection" "main" {
 resource "aws_route" "default-vpc-peer-route" {
   route_table_id            = var.default_vpc_rt
   destination_cidr_block    = var.cidr
-  vpc_peering_connection_id = "aws_vpc_peering_connection.main.id"
+  vpc_peering_connection_id = aws_vpc_peering_connection.main.id
 }
 
 ### Subnets
