ec03bca58e073a80778c00fbda9fcd0cc2aa4061
anandsa83 <65344631+anandsa83@users.noreply.github.com>
2025-09-11 10:21:37 +0530

    Changes

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -24,7 +24,7 @@ resource "aws_subnet" "web" {
   count              = length(var.web_subnets)  
   vpc_id             = aws_vpc.main.id
   cidr_block         = var.web_subnets[count.index]
-  availability_zones = var.availability_zones[count.index]
+  availability_zone = var.availability_zones[count.index]
 
   tags = {
     Name = "web-subnet-${spilt("-", var.availability_zones[count.index])[2]}"
@@ -36,7 +36,7 @@ resource "aws_subnet" "app" {
   count              = length(var.app_subnets)  
   vpc_id             = aws_vpc.main.id
   cidr_block         = var.app_subnets[count.index]
-  availability_zones = var.availability_zones[count.index]
+  availability_zone = var.availability_zones[count.index]
 
   tags = {
     Name = "app-subnet-${spilt("-", var.availability_zones[count.index])[2]}"
@@ -48,7 +48,7 @@ resource "aws_subnet" "db" {
   count              = length(var.db_subnets)  
   vpc_id             = aws_vpc.main.id
   cidr_block         = var.db_subnets[count.index]
-  availability_zones = var.availability_zones[count.index]
+  availability_zone = var.availability_zones[count.index]
 
   tags = {
     Name = "db-subnet-${spilt("-", var.availability_zones[count.index])[2]}"
@@ -60,7 +60,7 @@ resource "aws_subnet" "public" {
   count              = length(var.public_subnets)  
   vpc_id             = aws_vpc.main.id
   cidr_block         = var.public_subnets[count.index]
-  availability_zones = var.availability_zones[count.index]
+  availability_zone = var.availability_zones[count.index]
 
   tags = {
     Name = "public-subnet-${spilt("-", var.availability_zones[count.index])[2]}"
