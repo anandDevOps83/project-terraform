80b9d05d594dc316d2dcb5932876128cd65c6822
anandsa83 <65344631+anandsa83@users.noreply.github.com>
2025-09-18 10:18:57 +0530

    Changes

--- modules/load-balancer/main.tf
+++ modules/load-balancer/main.tf
@@ -59,11 +59,11 @@ resource "aws_lb_target_group" "main" {
 
 }
 
-resource "aws_lb_target_group_attachment" "main" {
-  target_group_arn = aws_lb_target_group.main.arn
-  target_id        = module.ec2.output.frontend_instance_id
-  port             = 80
-}
+#resource "aws_lb_target_group_attachment" "main" {
+#  target_group_arn = aws_lb_target_group.main.arn
+#  target_id        = module.ec2.output.frontend_instance_id
+#  port             = 80
+#}
 
 resource "aws_lb_listener" "public-http" {
   load_balancer_arn = aws_lb.main.arn
