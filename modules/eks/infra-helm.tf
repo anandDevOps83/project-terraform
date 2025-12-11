e0e3d13df7f9bdec3d98731a2b3e9783b7858dd8
anandsa83 <65344631+anandsa83@users.noreply.github.com>
2025-12-11 09:57:25 +0530

    Helm Error

--- modules/eks/infra-helm.tf
+++ modules/eks/infra-helm.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "kube-config" {
     depends_on = [aws_eks_node_group.main]
     provisioner "local-exec" {
-        command =<<EOF
+        command = <<EOF
 aws eks update-kubeconfig --name ${var.env}-eks
 kubectl apply -f /opt/vault-token.yaml
 EOF    
@@ -20,7 +20,7 @@ resource "helm_release" "external-secrets" {
 resource "null_resource" "external-secrets-store" {
     depends_on  = [helm_release.external-secrets]
     provisioner "local-exec" {
-        command =<<EOF
+        command = <<EOF
 kubectl apply -f - <<EOK
 apiVersion: external-secrets.io/v1beta1
 kind: ClusterSecretStore
